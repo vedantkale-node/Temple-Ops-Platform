0dc242a8e5f9e823751c460d4486f63ce0708d0d
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-07-10 21:30:43 +0530

    feat: add eslint to project

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    ignores: ["dist", "node_modules", "coverage"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+);
